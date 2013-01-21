c2c7411878604972c2d1c953bb145fbd5c003f26
Hoang Trieu Khang <hoangtrieukhang@gmail.com>
2013-01-21 10:29:37 +0700

    Initial Commit

---
+++ MiniApp/MAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MAAppDelegate.h
+//  MiniApp
+//
+//  Created by Trieu Khang on 1/21/13.
+//  Copyright (c) 2013 Hoang Trieu Khang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MiniApp/MAViewController.h
@@ -0,0 +1,13 @@
+//
+//  MAViewController.h
+//  MiniApp
+//
+//  Created by Trieu Khang on 1/21/13.
+//  Copyright (c) 2013 Hoang Trieu Khang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MAViewController : UIViewController
+
+@end
